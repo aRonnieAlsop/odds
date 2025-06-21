97387c74e1c97d434317892b988e8f5078ebdde9
Ronnie Alsop <alsop.arron@gmail.com>
2025-06-21 14:52:53 -0700

    'had to remember to add color coding for #s as on wheel'

--- src/rouletteStrategy.js
+++ src/rouletteStrategy.js
@@ -4,10 +4,27 @@ const wheel = [
   27, 10, 25, 29, 12, 8, 19, 31, 18, 6, 21, 33, 16, 4, 23, 35, 14, 2
 ];
 
+const redNumbers = new Set([
+  1, 3, 5, 7, 9, 12, 14, 16, 18,
+  19, 21, 23, 25, 27, 30, 32, 34, 36
+]);
+
 function spinWheel() {
   const index = Math.floor(Math.random() * wheel.length);
-  const result = wheel[index];
-  return result === 37 ? '00' : result;
+  const num = wheel[index];
+  const display = num === 37 ? '00' : num;
+  let color;
+
+  if (num === 0 || num === 37) {
+    color = 'green';
+  } else if (redNumbers.has(num)) {
+    color = 'red';
+  } else {
+    color = 'black';
+  }
+
+  console.log(`Spin result: ${display} (${color})`);
 }
 
-console.log("Spin result:", spinWheel());
+spinWheel();
+
